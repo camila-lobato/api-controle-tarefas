da4c6a1b751d1588c77de4afd68ccd2f9bd29ecd
camila <camilalmoreira9@gmail.com>
2025-09-09 21:34:20 -0400

    Create TarefaController.cs

---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServico.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+    }
+}
