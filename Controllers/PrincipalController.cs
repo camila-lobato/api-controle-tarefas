b6bedf6b252fd535215d26f23c25900daaf58978
camila <camilalmoreira9@gmail.com>
2025-09-09 20:38:53 -0400

    Arquivos iniciais da API

---
+++ Controllers/PrincipalController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServico.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class PrincipalController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Principal() 
+        {
+            var resultado = new { situacao = "Ok", mensagem = "API Serviço" };
+            return Ok(resultado);
+        }
+
+        [HttpGet("/Autor")]
+        public IActionResult Get()
+        {
+            var dados = new { nome = "Camila Lobato", 
+                telefone = "(19) 98835-3897)", 
+                email = "camilalmoreira9@gmail.com" };
+
+            return Ok(dados);
+        }
+
+    }
+
+
+}
---
+++ Models/Dtos/ChamadoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiServico.Models.Dtos
+{
+    public class ChamadoDto
+    {
+        // data transfer object
+
+        public required string Titulo { get; set; }
+        public required string Descricao { get; set; }
+    }
+}
